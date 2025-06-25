71bf3aa0542b0ab47eb18faf1a70ec14743daff2
Eniola Omotee <40490137+eniolaomotee@users.noreply.github.com>
2025-06-25 09:05:04 +0100

    token creation and tests

--- storeapi/security.py
+++ storeapi/security.py
@@ -5,7 +5,7 @@ from storeapi.database import database, user_table
 import datetime
 from jose import jwt, ExpiredSignatureError, JWTError
 from fastapi import HTTPException, status, Depends
-from typing import Annotated
+from typing import Annotated,Literal
 
 logger = logging.getLogger(__name__)
 # when you work on a backend api you look at 3 main things a.data to be stored b. data the api is going to recieve and c.return and implement the endpoint to the user.
@@ -16,23 +16,55 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-credentials_exception = HTTPException(
-    status_code=status.HTTP_401_UNAUTHORIZED,
-    detail="Could not validate Credentials",
-    headers={"WWW-Authenticate": "Bearer"},
-    
-)
+
+def create_credentials_exception(detail:str) -> HTTPException:
+    return HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=detail,
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+
 
 def access_token_expired_minutes() ->int:
     return 30
 
+
+def confirm_token_expired_minutes() ->int:
+    return 1440
+
+
+
 def create_access_token(email:str):
     logger.debug("Creating access token", extra={"email": email})
     expire = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=access_token_expired_minutes())
-    jwt_data = {"sub": email, "exp": expire}
+    jwt_data = {"sub": email, "exp": expire, "type": "access"}
     encoded_jwt = jwt.encode(jwt_data, key=SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+def create_confirmation_token(email:str):
+    logger.debug("Creating access token", extra={"email": email})
+    expire = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=confirm_token_expired_minutes())
+    jwt_data = {"sub": email, "exp": expire, "type": "confirmation"}
+    encoded_jwt = jwt.encode(jwt_data, key=SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def get_subject_from_token(token: str, type:Literal["access", "confirmation"]) -> str:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except ExpiredSignatureError as e:
+        raise create_credentials_exception("Token has expired") from e
+    except JWTError as e:
+        raise create_credentials_exception("Invalid token") from e
+    
+    email = payload.get("sub")
+    if email is None:
+        raise create_credentials_exception("Token is missing in sub field")
+    token_type = payload.get("type")
+    if token_type is None or token_type != "access":
+        raise create_credentials_exception("Token has incorrect type")
+    return email
+    
 
 def get_password_hash(password: str) -> str:
     return pwd_context.hash(password)
@@ -52,27 +84,14 @@ async def authenticate_user(email:str,password:str):
     logger.debug("Authenticating user", extra={"email": email})
     user = await get_user(email)
     if not user:
-        raise credentials_exception
+        raise create_credentials_exception("Invalid email or password")
     if not verify_password(password, user.password):
-        raise credentials_exception
+        raise create_credentials_exception("Invalid email or password")
     return user
 
 async def get_current_user(token:Annotated[str, Depends(oauth2_scheme)]):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        email = payload.get("sub")
-        if email is None:
-            raise credentials_exception
-    except ExpiredSignatureError as e:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token has expired",
-            headers={"WWW-Authenticate": "Bearer"},
-        ) from e
-    except JWTError as e:
-        raise credentials_exception from e
-    
+    email = get_subject_from_token(token, "access")
     user = await get_user(email)
     if user is None:
-        raise credentials_exception
+        raise create_credentials_exception("Could not find user for this token")
     return user
--- storeapi/tests/test_security.py
+++ storeapi/tests/test_security.py
@@ -6,11 +6,67 @@ from jose import jwt
 def test_access_token_expired_minutes():
     assert security.access_token_expired_minutes() == 30
     
+def test_confirm_token_expired_minutes():
+    assert security.confirm_token_expired_minutes() == 1440
+    
 def test_create_access_token():
     token = security.create_access_token("123")
     
-    assert {"sub":"123"}.items() <= jwt.decode(token, security.SECRET_KEY, algorithms=[security.ALGORITHM]).items()
+    assert {"sub":"123", "type":"access"}.items() <= jwt.decode(token, security.SECRET_KEY, algorithms=[security.ALGORITHM]).items()
 
+def test_create_confirmation_token():
+    token = security.create_confirmation_token("123")
+    
+    assert {"sub":"123", "type":"confirmation"}.items() <= jwt.decode(token, security.SECRET_KEY, algorithms=[security.ALGORITHM]).items()
+
+def test_get_subject_from_token_type_valid_confirmation():
+    email = "test@example.com"
+    token = security.create_access_token(email)
+    
+    assert email == security.get_subject_from_token(token, "confirmation")
+    
+def test_get_subject_from_token_type_valid_access():
+    email = "test@example.com"
+    token = security.create_access_token(email)
+    
+    assert email == security.get_subject_from_token(token, "access")
+
+def test_get_subject_from_token_expired(mocker):
+    mocker.patch("storeapi.security.access_token_expired_minutes", return_value=-1)
+    email = "test@example.com"
+    token = security.create_access_token(email)
+    
+    with pytest.raises(security.HTTPException) as exc_info:
+        security.get_subject_from_token(token, "access")
+    assert "Token has expired" == exc_info.value.detail
+
+def test_get_subject_from_token_invald():
+    token = "invalid_token"
+    
+    with pytest.raises(security.HTTPException) as exc_info:
+        security.get_subject_from_token(token, "access")
+        
+    assert "Invalid token" == exc_info.value.detail
+    
+    
+def test_get_subject_from_token_missing_sub():
+    email = "test@exmaple.com"
+    token = security.create_access_token(email)
+    payload = jwt.decode(token, security.SECRET_KEY, algorithms=[security.ALGORITHM])
+    del payload["sub"]
+    token = jwt.encode(payload, security.SECRET_KEY, algorithm=security.ALGORITHM)
+    
+    with pytest.raises(security.HTTPException) as exc_info:
+        security.get_subject_from_token(token, "access")
+    assert "Token is missing in sub field" == exc_info.value.detail
+    
+def test_get_subject_from_token_wrong_type():
+    email = "test@example.com"
+    token = security.create_confirmation_token(email)
+    with pytest.raises(security.HTTPException) as exc_info:
+        security.get_subject_from_token(token, "access")
+        
+    assert "Token has incorrect type" == exc_info.value.detail
 
 def test_password_hashes():
     password = "password"
@@ -62,3 +118,10 @@ async def test_get_current_user(registered_user: dict):
 async def test_get_current_user_invalid_token():
     with pytest.raises(security.HTTPException):
         await security.get_current_user("invalid_token")
+        
+@pytest.mark.anyio
+async def test_get_current_user_wrong_type_token(registered_user: dict):
+    token = security.create_confirmation_token(registered_user["email"])
+    
+    with pytest.raises(security.HTTPException):
+        await security.get_current_user(token)
